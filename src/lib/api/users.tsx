daa1ec2f0d2fc47377f7e54a7612de7da07ceb7f
gabcadi <gcastroa16@gmail.com>
2025-07-15 17:43:01 -0600

    feat: add user API routes for fetching all users and user by ID

---
+++ src/lib/api/users.tsx
@@ -0,0 +1,17 @@
+import api from '@/lib/api/axiosInstance';
+
+export const getUsers = async () => {
+  const res = await api.get('/users');
+  if (res.status !== 200) {
+    throw new Error('Error fetching users');
+  }
+  return res.data;  
+};
+
+export const getUserById = async (id: string) => {
+  const res = await api.get(`/users/${id}`);
+  if (res.status !== 200) {
+    throw new Error(`Error fetching user with ID ${id}`);
+  }
+  return res.data;
+};
